c7ea8a3bacbce92f5cacf3460339fb62d3826267
Luca De Vitis <luca@monkeython.com>
2011-07-28 00:44:59 +0200

    Code cleanup.

--- python-musixmatch/setup.py
+++ python-musixmatch/setup.py
@@ -9,27 +9,20 @@ sys.path.insert(1, wd)
 name = 'musixmatch'
 pkg = __import__(name)
 author, email = pkg.__author__.rsplit(' ', 1)
-email = email.strip('<>')
 
-url='http://src.monkeython.org/'
-version = pkg.__version__
-classifiers = pkg.__classifiers__
-readme = open(os.path.join(wd, 'README.rst'),'r').readlines()
-description = readme[0]
-long_description = '\n'.join(readme[2:])
+with open(os.path.join(wd, 'README.rst'),'r') as readme:
+    long_description = readme.read()
 
 setup(
-    name=name,
-    version=version,
-    author=author,
-    author_email=email,
-    maintainer=author,
-    maintainer_email=email,
-    url='%s/doc/%s' % (url, name),
-    description=description,
-    long_description=long_description,
-    download_url='%s/eggs/%s' % (url, name),
-    classifiers=classifiers,
-    packages=[name],
-    test_suite='tests.suite')
+    name = name,
+    version = pkg.__version__,
+    author = author,
+    author_email = email.strip('<>'),
+    url = 'http://projects.monkeython.com/musixmatch/python-musixmatch/html',
+    description = "Package to interface with the Musixmatch API",
+    long_description = long_description,
+#    download_url = '%s/eggs/%s' % (url, name),
+    classifiers = pkg.__classifiers__,
+    packages = [name],
+    test_suite = 'tests.suite')
 
